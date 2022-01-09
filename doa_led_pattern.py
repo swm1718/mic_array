067fccbfbcdd6c4e4e8191d4e974787e353260d5
swm1718 <52105686+swm1718@users.noreply.github.com>
2022-01-09 00:32:37 +0000

    Update doa_led_pattern.py
    Rounding number to integer

--- doa_led_pattern.py
+++ doa_led_pattern.py
@@ -18,7 +18,7 @@ class DOALEDPattern(object):
     def wakeup(self, direction=0):
         position = int((direction + 15) / (360 / self.pixels_number)) % self.pixels_number
 
-        pixels = [0, 24, 0, 24, 0, 0, 24, 0] * (self.pixels_number/2)
+        pixels = [0, 24, 0, 24, 0, 0, 24, 0] * round(self.pixels_number/2)
         #pixels[position * 4 + 2] = 48
 
         self.show(pixels)
